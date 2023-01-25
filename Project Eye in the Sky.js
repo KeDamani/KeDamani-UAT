a56069a2efb9e2905d04f14cd010540c4cb9eae4
Kenyatta <109622225+KeDamani@users.noreply.github.com>
2023-01-24 22:09:10 -0600

    Add files via upload

---
+++ Project Eye in the Sky.js
@@ -0,0 +1,22 @@
+
+/* prepare
+to be 
+amazed
+*/
+
+function JSAlert()
+{
+    alert("JS Alert")
+}
+
+function JSUpdate()
+{
+    alert("JSU");
+    document.getElementById("JSU").innerHTML = "cross your fingers";
+}
+
+function add(num7, num9)
+{
+var sum = parselnt(num7) + parseInt(nu9);
+alert(sum)
+}
